473ecce61029454228a5d3d17d7c7fd8eb3b73aa
Rivaldo Esteban <63760024+RivaldoEsteban@users.noreply.github.com>
2021-07-08 17:28:15 -0500

    agregando proyectos

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,78 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import Networds from "./networks";
+import "../css/hero.css";
 const HeroStyled = styled.section`
   height: 700px;
   width: 100vw;
   display: flex;
   background: #0f0e17;
   position: relative;
-  .hero-image {
-    height: inherit;
-  }
-  .hero-image img {
-    width: calc(50% + 100px);
-    position: absolute;
-    right: 0;
-    object-fit: cover;
-    height: inherit;
-    z-index: 1;
-  }
-  .hero-right {
-    height: inherit;
-    display: flex;
-    width: 50%;
-    flex-direction: column;
-  }
-  .clip-path {
-    clip-path: polygon(0% 0%, calc(100% - 100px) 0%, 100% 100%, 0 100%);
-    background: #0f0e17;
-    height: inherit;
-    width: 50%;
-    z-index: 3;
-    position: relative;
-  }
-  .hero-content {
-    margin: 20px 120px 20px 50px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    height: inherit;
-    color: white;
-  }
-  .hero-p {
-    margin: 0;
-    color: #f25f4c;
-    font-size: 15px;
-  }
-  h1 {
-    font: 700 normal 55px/60px var(--fontTitle);
-    margin: 0;
-    margin: 1rem 0 0 0;
-  }
-  p {
-    margin: 0 0 1rem 0;
-    color: #a7a9be;
-    font-size: 18px;
-  }
-  strong {
-    font-size: 20px;
-    margin-bottom: 1rem;
-  }
-  .curriculum {
-    background: #f25f4c;
-    border: none;
-    padding: 12px 0;
-    width: 100%;
-    margin: 1rem auto 2rem;
-    color: white;
-    font: 700 normal 1rem/18px var(--fontTitle);
-  }
-  @media (max-width: 850px) {
-    h1 {
-      font-size: 48px;
-    }
-  }
 `;
 
 function Hero() {
@@ -88,7 +23,7 @@ function Hero() {
           </p>
           <h1>Soy Rivaldo Esteban</h1>
           <strong>Frontend Developer </strong>
-          <p>
+          <p className="me-description">
             Me encanta centrarme en los detalles de las experiencias de nuevos
             productos con una implementación en código perfecta. Pero los
             grandes productos no comienzan ahí, comienzan con una sólida
--- src/project-data.js
+++ src/project-data.js
@@ -2,7 +2,7 @@ const projectData = [
   {
     title: "Spotifu",
     description:
-      "Spotifu plataforma de música, escoge la playlist que quieras y disfruta de su contenido... , cada canción es especial y te hara vibrar segundo a segundo.",
+      "Spotifu plataforma de música, escoge la playlist que quieras y disfruta de su contenido. Cada canción es especial y te hara vibrar segundo a segundo.",
     background: "background-project/spotifu.png",
     gif: "gif/spotifu.gif",
     linkProject: "https://github.com/RivaldoEsteban/Spotifu-react",
@@ -38,6 +38,109 @@ const projectData = [
     linkProject: "https://github.com/RivaldoEsteban/marvol-react",
     linkRepo: "https://rivaldoesteban.github.io/marvol-react/",
     id: "project-4",
+  },
+  {
+    title: "Rick and Morty",
+    description:
+      "Rick y Morty es una serie de televisión estadounidense de animación para adultos creada por Justin Roiland y Dan Harmon en 2013 para Adult Swim. ",
+    background: "background-project/rick-and-morty.png",
+    gif: "gif/rick-and-morty.gif",
+    linkProject: "https://rivaldoesteban.github.io/rick-and-morty-react/",
+    linkRepo: "https://github.com/RivaldoEsteban/rick-and-morty-react",
+    id: "project-5",
+  },
+  {
+    title: "Cuenta regresiva",
+    description:
+      "Temporizador de cuenta regresiva en vivo, pon la fecha futura y espera a que el tiempo se completa, este proyecto fue creado para aprender a usar new Datee",
+    background: "background-project/countdown.png",
+    gif: "gif/countdown.gif",
+    linkProject:
+      "https://rivaldoesteban.github.io/countdown-component/index.html",
+    linkRepo: "https://github.com/RivaldoEsteban/countdown-component",
+    id: "project-6",
+  },
+  {
+    title: "Sidebar",
+    description:
+      "El sidebar es un componente comun en una página web y con este proyecto se trato de simular lo mas posible la animacion y las redirecciones ",
+    background: "background-project/sidebar.png",
+    gif: "gif/sidebar.gif",
+    linkProject:
+      "https://rivaldoesteban.github.io/sidebar-component/index.html",
+    linkRepo: "https://github.com/RivaldoEsteban/sidebar-component",
+    id: "project-7",
+  },
+  {
+    title: "Reproductor de video",
+    description:
+      "Con este proyecto se logro aprender a usar los metodos de video, y logre pausar, reproducir,adelantar,retroceder y una barra del tiempo actual del video funcional.",
+    background: "background-project/video.png",
+    gif: "gif/video.gif",
+    linkProject: "https://rivaldoesteban.github.io/video-componnet/index.html",
+    linkRepo: "https://github.com/RivaldoEsteban/video-componnet",
+    id: "project-8",
+  },
+  {
+    title: "Hero component",
+    description:
+      "El hero en un proyecto es importante porque es la forma de implementar imágenes y textos atractivos para el usuario. ",
+    background: "background-project/hero.png",
+    gif: "gif/hero.gif",
+    linkProject: "https://rivaldoesteban.github.io/hero-component/index.html",
+    linkRepo: "https://github.com/RivaldoEsteban/hero-component",
+    id: "project-9",
+  },
+  {
+    title: "Calculadora",
+    description:
+      "Este proyecto me sirvio para ser mas logico y replicar las acciones dee una calculadora. Haz tus operaciones.",
+    background: "background-project/calculator.png",
+    gif: "gif/calculator.gif",
+    linkProject:
+      "https://rivaldoesteban.github.io/calculator-component/index.html",
+    linkRepo: "https://github.com/RivaldoEsteban/calculator-component",
+    id: "project-10",
+  },
+  {
+    title: "Calec",
+    description:
+      "Calel es su centro de programación de reuniones de manera profesional y eficiente y elimina el problema del intercambio de correos electrónicos para que pueda dedicar más tiempo a su trabajo.",
+    background: "background-project/calel.png",
+    gif: "gif/calel.gif",
+    linkProject: "https://rivaldoesteban.github.io/Project-Calel/",
+    linkRepo: "https://github.com/RivaldoEsteban/Project-Calel",
+    id: "project-11",
+  },
+  {
+    title: "Striped",
+    description:
+      "Millones de empresas de todos los tamaños, desde nuevas empresas hasta grandes empresas, utilizan el software y las API de Stripe para aceptar pagos, enviar pagos y administrar sus negocios en línea.",
+    background: "background-project/striped.png",
+    gif: "gif/striped.gif",
+    linkProject: "https://rivaldoesteban.github.io/project-striped/index.html",
+    linkRepo: "https://github.com/RivaldoEsteban/project-striped",
+    id: "project-12",
+  },
+  {
+    title: "Skyline",
+    description:
+      "El primer editor completamente potenciado por inteligencia artificial. Mas posibilidades , menos rutinas y sin barreras de aprendisaje.",
+    background: "background-project/skyline.png",
+    gif: "gif/skyline.gif",
+    linkProject: "https://rivaldoesteban.github.io/project-skyline/index.html",
+    linkRepo: "https://github.com/RivaldoEsteban/project-skyline",
+    id: "project-12",
+  },
+  {
+    title: "Nemium",
+    description:
+      "Este proyecto lo realice tomando el curso : Curso ensencial de HTML Y CSS de  https://leonidasesteban.com/aprender/html-css-fundamentos",
+    background: "background-project/nemium.png",
+    gif: "gif/nemium.gif",
+    linkProject: "https://rivaldoesteban.github.io/Project-Nemium/nemiumA.html",
+    linkRepo: "https://github.com/RivaldoEsteban/Project-Nemium",
+    id: "project-13",
   },
 ];
 
